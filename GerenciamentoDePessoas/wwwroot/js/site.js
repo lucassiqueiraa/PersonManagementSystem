a80142a4c72adb5f38e8d9e7fa9badba5218385b
Lucas siqueira <lucassiqueira0763@hotmail.com>
2025-09-08 11:19:07 +0100

    feat: Add DataTables integration with pagination, filtering, and listing; update favicon with custom icon

--- GerenciamentoDePessoas/wwwroot/js/site.js
+++ GerenciamentoDePessoas/wwwroot/js/site.js
@@ -1,4 +1,12 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
+﻿jQuery.noConflict();
+(function ($) {
+    $(document).ready(function () {
+        $('.tabela-pessoas').DataTable(
+            {
+                language: {
+                    url: 'https://cdn.datatables.net/plug-ins/2.3.3/i18n/pt-PT.json',
+                }
+            }
+        );
+    });
+})(jQuery);
